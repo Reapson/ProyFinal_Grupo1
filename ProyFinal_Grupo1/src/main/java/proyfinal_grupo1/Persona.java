611ec37fb3d8ac62b39277ce266b8065e0918f0d
Ripichip <josueanticona@gmail.com>
2024-02-14 14:59:58 -0600

    Creacion del main y clase Persona
    Creacion del main y clase Persona, se inicia a crear el esqueleto del programa

---
+++ ProyFinal_Grupo1/src/main/java/proyfinal_grupo1/Persona.java
@@ -0,0 +1,88 @@
+//Clase Abstracta Persona, va a ser la clase padre de Empleado, Cliente y Proveedor
+
+package proyfinal_grupo1;
+
+public abstract class Persona {
+    //Variables
+    private int numeroCedula;
+    private String nombre;
+    private String primerApellido;
+    private String provincia;
+    private String canton;
+    private String correo;
+    private double numTelefono;
+
+    //Constructor
+    public Persona(int numeroCedula, String nombre, String primerApellido, String provincia, String canton, String correo, double numTelefono) {
+        this.numeroCedula = numeroCedula;
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.provincia = provincia;
+        this.canton = canton;
+        this.correo = correo;
+        this.numTelefono = numTelefono;
+    }
+
+    //Getters y Setters
+    public int getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public double getNumTelefono() {
+        return numTelefono;
+    }
+
+    public void setNumTelefono(double numTelefono) {
+        this.numTelefono = numTelefono;
+    }
+    
+    //Metodos abstractos de la clase Persona
+    public abstract void insertarPersona();
+    public abstract void leerPersonas();
+    public abstract void editarPersona();
+    public abstract void eliminarPersona();
+}
---
+++ ProyFinal_Grupo1/src/main/java/proyfinal_grupo1/main.java
@@ -0,0 +1,23 @@
+/*
+UNIVERSIDAD FIDELITAS
+INGENIERIA EN SISTEMAS DE COMPUTACION
+PROGRAMACION CLIENTE-SERVIDOR
+
+PROYECTO FINAL
+"Sistema para la administración de una tienda de reparaciones de equipos de cómputo"
+
+Integrantes:
+Reapson Anticona Corrales
+Rossemary Carballo Anticona
+Jeremias Carballo Chavarria
+*/
+
+
+package proyfinal_grupo1;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
